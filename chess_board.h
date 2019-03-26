3b5ce9ec1e7e76d45de36f90f53c5b3b2c4d408f
Michael Evan Wermert <mwermert@hydra11.eecs.utk.edu>
2019-03-26 17:03:04 -0400

    updated chessboard.h

--- chess_board.h
+++ chess_board.h
@@ -1,5 +1,7 @@
 #ifndef CHESS_BOARD_H
 #define CHESS_BOARD_H
+#include <vector>
+#include "Piece.h"
 
 class ChessBoard
 {
@@ -7,11 +9,12 @@ public:
     ChessBoard();
     ~ChessBoard();
     void Print();
+	void move(int rowFrom, int colFrom, int rowTo, int colTo);
 
 private:
     vector<Piece> blacks;
     vector<Piece> whites;
-    vector<vector<Piece>> board;
+    vector<vector <Piece*> > board;
 };
 
 #endif
