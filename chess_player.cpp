6ea525eca6503f08967b8dc917e8f56d7520482c
mwermert <46689709+mwermert@users.noreply.github.com>
2019-03-26 17:21:12 -0400

    Update chess_player.cpp

--- chess_player.cpp
+++ chess_player.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-void PrintBoard(vector < vector <string> > &board);
-
 void InitializeBoard( vector < vector <string> > &board);
 
 void TwoPlayerMode();
@@ -116,41 +114,6 @@ void TwoPlayerMode(){
     }
 
 }
-
-
-
-void PrintBoard(vector < vector <string> > &board){
-
-	string solidRow = "\t  #########################################";
-	string buffRow  = "\t  #    #    #    #    #    #    #    #    #";
-
-
-	cout<<endl;
-	cout<<"\t     1";
-	for(int i = 2;i <= 8;i++)
-		cout<<"    "<<i;
-	cout<<endl;
-	cout<<solidRow<<endl;
-	char rowCount = 'A';
-	
-
-	for(int i = 0; i < board.size();i++){
-
-		cout<<buffRow<<endl;
-		cout<<"\t"<<rowCount<<" #";
-		for(int j = 0;  j < board[i].size();j++){
-			
-			cout<<" "<<board[i][j]<<" #";
-
-		}
-		cout<<endl;
-		cout<<buffRow<<endl;
-		cout<<solidRow<<endl;
-
-		rowCount++;
-	}
-}
-
 
 //initializes board at the beginning of the game
 void InitializeBoard( vector < vector <string> > &board){
