3a6cfde4d20d2e93a3fafeb6d02af94d896bf9ae
Michael Evan Wermert <mwermert@hydra11.eecs.utk.edu>
2019-03-31 23:02:47 -0400

    Added Pawn and Knight Movement

--- chess_board.h
+++ chess_board.h
@@ -10,6 +10,11 @@ public:
     ~ChessBoard();
     void Print();
     void Move(int rFrom, int cFrom, int rTo, int cTo);
+	char getSpaceColor(int r, int c);
+	char getSpaceType(int r, int c);
+	bool checkNull(int r, int c);
+	Piece *getPiece(int r, int c);
+	std::vector<std::vector <Piece*> > getBoard();
 
 private:
     std::vector<Piece*> blacks;
--- knight.cpp
+++ knight.cpp
@@ -1,4 +1,55 @@
 #include "piece.h"
 Knight::Knight(int row, int col, char color):Piece(row, col, color, 'N')
 {
+
+}
+
+void Knight::ValidMoves(std::multimap<char, int>&moves, const std::vector < std::vector <Piece*> > board){
+
+	
+
+	if(row - 1 >= 0 && col - 2 >= 0){
+		if(board[row - 1][col-2] == nullptr || board[row - 1][col-2]->GetColor() != board[row][col]->GetColor())
+			moves.insert ( std::pair<char,int>(row - 1 + 'A',col-2) );
+	}
+
+	if(row - 2 >= 0 && col - 1 >= 0){
+		if(board[row - 2][col-1] == nullptr || board[row - 2][col-1]->GetColor() != board[row][col]->GetColor())
+			 moves.insert ( std::pair<char,int>(row - 2 + 'A',col-1) );
+	}
+
+	if(row + 1 < (int)board.size() && col - 2 >= 0){
+		if(board[row +1][col-2] == nullptr || board[row +1][col-2]->GetColor() != board[row][col]->GetColor())
+		 moves.insert ( std::pair<char,int>(row + 1 + 'A',col-2) );
+	}
+
+	if(row + 2 < (int)board.size() && col - 1 >= 0){
+		if(board[row + 2][col-1] == nullptr || board[row + 2][col-1]->GetColor() != board[row][col]->GetColor())
+			 moves.insert ( std::pair<char,int>(row + 2 + 'A',col-1) );
+	}
+
+
+	if(row - 1 >= 0 && col + 2 < (int)board[0].size()){
+		if(board[row - 1][col+2] == nullptr || board[row - 1][col+2]->GetColor() != board[row][col]->GetColor())
+			 moves.insert ( std::pair<char,int>(row - 1 + 'A',col+2) );
+	}
+
+
+	if(row - 2 >= 0 && col + 1 < (int)board[0].size()){
+		if(board[row - 2][col+1] == nullptr || board[row - 2][col+1]->GetColor() != board[row][col]->GetColor())
+			moves.insert ( std::pair<char,int>(row - 2 + 'A',col+1) );
+	}
+
+	
+	if(row + 1 < (int)board.size() && col + 2 < (int)board[0].size()){
+		if(board[row + 1][col+2] == nullptr || board[row + 1][col+2]->GetColor() != board[row][col]->GetColor())
+			 moves.insert ( std::pair<char,int>(row + 1 + 'A',col+2) );
+	}
+
+
+	if(row + 2 < (int)board.size() && col + 1 < (int)board[0].size()){
+		if(board[row + 2][col+1] == nullptr || board[row + 2][col+1]->GetColor() != board[row][col]->GetColor())
+			moves.insert ( std::pair<char,int>(row + 2 + 'A',col+1) );
+	}	
+
 }
